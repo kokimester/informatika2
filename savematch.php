e7c126389d8968547604d304d4a9570d09a01938
Fejes Daniel <fejesdani@gmail.com>
2021-04-30 22:14:12 +0200

    savematch first commit

---
+++ savematch.php
@@ -0,0 +1,13 @@
+<?php
+
+$sList = $_POST['selfList'];
+$sShotDown = $_POSTp['selfShotDown'];
+$oShotDown = $_POST['opponentShotDown'];
+
+if($sShotDown > 200 || $oShotDown > 200)
+{
+    
+}
+
+
+?>
