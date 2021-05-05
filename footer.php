5fdadae3e186a7dfa75e420c433dd1f42263cb5b
Fejes Daniel <fejesdani@gmail.com>
2021-05-05 22:45:06 +0200

    added footer

--- footer.php
+++ footer.php
@@ -3,5 +3,12 @@
     integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" 
     crossorigin="anonymous"></script>
   
+  <
+<div id="footer" class="footer mt-auto py-3 bg-light fixed-bottom text-center">
+  <div class="container">
+    <span class="text-muted">Join our discord server! </span> <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">https://discord.gg/D2NAeSwV</a>
+  </div>
+</div>
+
 </body>
 </html>
