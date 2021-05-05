0dc5aba7357aff16876057b5dc418be0dcb32c73
Fejes Daniel <fejesdani@gmail.com>
2021-05-05 22:44:46 +0200

    deleted unused parts

--- db.php
+++ db.php
@@ -9,73 +9,38 @@ function opendb(){
 function init_db(){
     $link = mysqli_connect("localhost","root","") or die("Connection failure" . mysqli_error($link));
     mysqli_select_db($link,"hf");
-    $statement = "#tabla az alap adatokhoz
-    CREATE TABLE if not exists telefonkonyv_base(
+
+
+    $user = "CREATE TABLE if not exists user(
         id int primary key auto_increment,
         nev varchar(100),
-        szam varchar(100)
-    );
+        email varchar(100) unique,
+        jelszo varchar(100),
+        steamid varchar(40) unique,
+        discordid varchar(40) unique,
+        elo int DEFAULT 1500
+    );";
+    $merkozes = "CREATE TABLE if not exists merkozes(
+        id int primary key auto_increment,
+        player_1_id int,
+        player_1_points int,
+        player_1_list varchar(200),
+        player_1_confirmed boolean default false,
+        FOREIGN KEY (player_1_id) REFERENCES user(id),
+        player_2_id int,
+        player_2_points int,
+        player_2_list varchar(200),
+        player_2_confirmed boolean default false,
+        FOREIGN KEY (player_2_id) REFERENCES user(id),
+        elo_change int
+        );";
     
-    #itt kerul be a cim
-    CREATE TABLE if not exists telefonkonyv_ext(
-        cim varchar(200),
-        telefonkonyv_base_id int NOT NULL,
-        FOREIGN KEY (telefonkonyv_base_id) REFERENCES telefonkonyv_base(id)
-    );
-    
-    #view a ket tabla miatt
-    CREATE OR REPLACE VIEW telefonkonyv
-    AS
-    SELECT tb.id, tb.nev, tb.szam, cim 
-    FROM telefonkonyv_base tb 
-    LEFT OUTER JOIN telefonkonyv_ext te ON te.telefonkonyv_base_id = tb.id;
-    
-    DROP PROCEDURE IF EXISTS ADD_ENTRY;
-    DROP PROCEDURE IF EXISTS DELETE_ENTRY;
-    
-    DELIMITER $$
-    #procedura a ket tablas beszurashoz (view miatt)
-    CREATE PROCEDURE ADD_ENTRY(IN Pnev varchar(100),IN Pszam varchar(100),IN Pcim varchar(200))
-    BEGIN
-    DECLARE temp_id INT DEFAULT 0;
-    
-    #ha megszakad a procedure akkor ezzel az error handlerrel rollbackel
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
-        BEGIN
-              ROLLBACK;
-        END;
-    
-    #tranzakcioval lekezeljuk ha nem sikerul valami
-    START TRANSACTION;
-    SET autocommit = 0;
-    INSERT INTO telefonkonyv_base(nev,szam) VALUES(Pnev, Pszam);
-    SELECT id INTO temp_id FROM telefonkonyv_base tb WHERE nev=Pnev AND tb.szam=Pszam ;
-    INSERT INTO telefonkonyv_ext(telefonkonyv_base_id,cim) VALUES(temp_id, Pcim);
-    SET autocommit = 1;
-    COMMIT;
-    END$$
-    
-    #torleshez is letrehozunk proceduret mert ket tabla van
-    CREATE PROCEDURE DELETE_ENTRY(IN Pid INT)
-    BEGIN
-    
-    #ha megszakad a procdeure akkor ezzel az error handlerrel rollbackel
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
-        BEGIN
-              ROLLBACK;
-        END;
-    
-    #tranzakcioval lekezeljuk ha nem sikerul valami
-    START TRANSACTION;
-    DELETE FROM telefonkonyv_ext WHERE telefonkonyv_base_id = Pid;
-    DELETE FROM telefonkonyv_base WHERE id=Pid;
-    COMMIT;
-    END$$
-    
-    DELIMITER ;
-    
-    ";
-    mysqli_query($link, $statement);
+    $insert1 = "INSERT INTO user(nev,email,jelszo,elo) VALUES('Jozsi','jozsi@gmail.hu','7217359295a9ae727cb667220d713adc',1500);"; // AAAAaaaa2
+    $insert2 = "INSERT INTO user(nev,email,jelszo,elo) VALUES('Jancsi','jancsi@gmail.hu','24a5a37e074d43f54d3d6e033d86886e',1500);"; // juliska
+    mysqli_query($link, $user);
+    mysqli_query($link, $merkozes);
+    mysqli_query($link, $insert1);
+    mysqli_query($link, $insert2);
     mysqli_close($link);
 
 }
